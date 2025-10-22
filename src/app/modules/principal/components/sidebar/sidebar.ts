44abfc4ef8860141247e187d5e7addc9ee78d090
David Jaramillo <djdavidjaramillo@gmail.com>
2025-10-22 16:45:41 -0500

    welcomePage

--- src/app/modules/principal/components/sidebar/sidebar.ts
+++ src/app/modules/principal/components/sidebar/sidebar.ts
@@ -9,40 +9,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar.html'
 })
 export class Sidebar {
-    expandedItems: string[] = [];
 
-  menuItems = [
-    {
-      title: 'Inicio',
-      href: '/inicio',
-      icon: 'fas fa-home',
-    },
-    {
-      title: 'Salud',
-      icon: 'fas fa-heartbeat',
-      submenu: [
-        { title: 'Signos Vitales', href: '/salud/signos', icon: 'fas fa-stethoscope' },
-        { title: 'Historial Médico', href: '/salud/historial', icon: 'fas fa-notes-medical' },
-      ],
-    },
-    {
-      title: 'Recordatorios',
-      href: '/recordatorios',
-      icon: 'fas fa-bell',
-    },
 
-  ];
-
-  toggleExpanded(title: string) {
-    const index = this.expandedItems.indexOf(title);
-    if (index > -1) {
-      this.expandedItems.splice(index, 1);
-    } else {
-      this.expandedItems.push(title);
-    }
-  }
-
-  isExpanded(title: string): boolean {
-    return this.expandedItems.includes(title);
-  }
 }
